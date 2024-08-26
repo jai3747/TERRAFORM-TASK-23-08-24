cefb50912d2cb4d953555d91bd0421833a744b79
Jayachandran <159675826+jai3747@users.noreply.github.com>
2024-08-26 13:39:34 +0530

    Update backend.tf

--- modules/backend.tf
+++ modules/backend.tf
@@ -9,13 +9,3 @@ terraform {
   }
 }
 
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "terraform-state-lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
